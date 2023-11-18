dcb652ef76fbda507eec843f41997f1932816faf
natnael772 <natnaeldeyas0@gmail.com>
2023-11-18 16:17:18 +0300

    Date counter

---
+++ src/DateCounter.jsx
@@ -0,0 +1,31 @@
+import { useReducer, useState } from "react";
+
+function DateCounter() {
+  return (
+    <div className="counter">
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={defineStep}
+        />
+        <span>{step}</span>
+      </div>
+
+      <div>
+        <button onClick={dec}>-</button>
+        <input value={count} onChange={defineCount} />
+        <button onClick={inc}>+</button>
+      </div>
+
+      <p>{date.toDateString()}</p>
+
+      <div>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
+  );
+}
+export default DateCounter;
