5c168c435279e502e664a930b82bbc8e34597312
natnael772 <natnaeldeyas0@gmail.com>
2023-11-21 10:01:10 +0300

    Handle start button click

--- src/StartScreen.jsx
+++ src/StartScreen.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-export default function StartScreen({ numQuestions }) {
+export default function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
       <h2>Welcome to React Quiz!</h2>
       <h3>{numQuestions} question to test your React mastery</h3>
-      <button className="btn btn-ui">Let's start</button>
+      <button className="btn btn-ui" onClick={dispatch}>
+        Let's start
+      </button>
     </div>
   );
 }
