45ff0fac0ee9fb7fccc4e9cc709975d2e8d8ebc7
natnael772 <natnaeldeyas0@gmail.com>
2023-11-21 09:31:46 +0300

    App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import { useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import DateCounter from "./DateCounter";
+import Header from "./Header";
+import MainSection from "./MainSection";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 
 function App() {
-  return <></>;
+  return (
+    <div className="app">
+      <Header />
+      <MainSection>
+        <p>1/15</p>
+        <p>Question?</p>
+      </MainSection>
+    </div>
+  );
 }
 
 export default App;
